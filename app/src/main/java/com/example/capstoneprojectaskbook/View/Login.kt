608c97832f746ea82c7806a56ddab3d3ee006590
DedikHasanah939 <dedikhasanah939@gmail.com>
2024-06-06 11:28:09 +0700

    UI Slicing Askbook

---
+++ app/src/main/java/com/example/capstoneprojectaskbook/View/Login.kt
@@ -0,0 +1,12 @@
+package com.example.capstoneprojectaskbook.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstoneprojectaskbook.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
