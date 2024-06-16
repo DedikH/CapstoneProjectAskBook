8e6062d7e042a405ae6ee74b68e20a7304b844da
Prasetya Aji (ej) <prasetyayudhistira77@gmail.com>
2024-06-16 08:05:18 +0700

    Slicing UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.capstoneprojectaskbook"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.capstoneprojectaskbook"
@@ -41,7 +41,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
